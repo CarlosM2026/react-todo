d67f03970be685f4a283ed599302daaf0ebb6201
CarlosM2026 <carlosmoncada2022@gmail.com>
2024-07-23 13:06:10 -0400

    Lesson 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,10 +7,21 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 
+const useSemiPersistentState = () => {
+  const [todoList,setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')));
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  }, [todoList])
+
+  return [todoList, setTodoList]
+
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
-  const [todoList,setTodoList] = useState([]);
+  const [todoList,setTodoList] = useSemiPersistentState();
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
   }
